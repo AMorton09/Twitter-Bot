b444944ee411a69f25f04af5edc2b020be8b9186
Andrew Morton <morton09@me.com>
2017-08-08 10:58:54 -0500

    bugs in code

--- app.js
+++ app.js
@@ -5,10 +5,33 @@ var twitter = new twit(config);
 
 var retweet = function() {
     var params = {
-        q: '#nodejs, #Nodejs',
+        q: '#testing12345',
         result_type: 'recent',
         lang: 'en'
     }
+    twitter.get('search/tweets', params, function(err, data) {
+        // if there no errors
+        if (!err) {
+            // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+            console.log('Something went wrong while SEARCHING...');
+        }
+    });
 }
 
 
@@ -20,28 +43,9 @@ var favoriteTweet = function() {
     }
 }
 
-twitter.get('search/tweets', params, function(err, data) {
-    // if there no errors
-    if (!err) {
-        // grab ID of tweet to retweet
-        var retweetId = data.statuses[0].id_str;
-        // Tell TWITTER to retweet
-        twitter.post('statuses/retweet/:id', {
-            id: retweetId
-        }, function(err, response) {
-            if (response) {
-                console.log('Retweeted!!!');
-            }
-            // if there was an error while tweeting
-            if (err) {
-                console.log('Something went wrong while RETWEETING... Duplication maybe...');
-            }
-        });
-    }
-    // if unable to Search a tweet
-    else {
-        console.log('Something went wrong while SEARCHING...');
-    }
-});
 
 
+// grab & retweet as soon as program is running...
+retweet();
+// retweet in every 50 minutes
+setInterval(retweet, 3000000);
