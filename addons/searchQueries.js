a9e039a0c327e474df9a17e2897c0bf509df5d78
Andrew Morton <morton09@me.com>
2017-08-21 15:33:38 -0500

    following almost works

---
+++ addons/searchQueries.js
@@ -0,0 +1,7 @@
+module.exports = {
+    vapenation: {
+        q: '#VapeNation',
+        result_type: 'recent',
+        lang: 'en'
+    }
+}
--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const twit = require("twit");
+const twit = require('twit');
 const config = require('./config.js');
 const bot = new twit(config);
+const queries = require('./addons/searchQueries.js')
 const facts = require('./addons/facts.json')
+const followBotModule = require('node-twitter-bot');
+const followBot = new followBotModule({
+    consumerKey : config.consumer_key,
+    consumerSecret : config.consumer_secret,
+    accessToken : config.access_token,
+    accessTokenSecret : config.access_token_secret
+});
 
 var randomProperty = function (obj) {
     let keys = Object.keys(obj);
     return obj[keys[ keys.length * Math.random() << 0]];
 };
 
-var retweet = function() {
-    var params = {
-        q: '#VapeNation',
-        result_type: 'recent',
-        lang: 'en'
-    }
+var tweetThis = function(message){
+    bot.post('statuses/update', { status: message }, function(err, data) {
+        console.log(data)
+    })
+}
+
+
+
+var retweet = function(params) {
+
     bot.get('search/tweets', params, function(err, data) {
         // if there no errors
         if (!err) {
@@ -39,23 +51,44 @@ var retweet = function() {
     });
 }
 
-
-var  randTweet = function () {
-    var tweetBody = randomProperty(facts)
-    bot.post('statuses/update', { status: tweetBody }, function(err, data, response) {
-        console.log(data)
+//follow worm
+var followWorm = function () {
+    /*var searchParams = {
+        q: '#VapeNation',
+        result_type: 'recent',
+        lang: 'en'
+    }
+    bot.get('search/tweets', searchParams, function(err, data) {
+        console.log(data.statuses[0].user)
     })
+    */
+    var options = {
+        result_type: String,
+        count: 1,
+    }
+    var phrase = "#VapeNation";
+    followBot.followByTweets(phrase,options)
+}
+
+
+//function to tweet a random tweet from facts.json
+var  randTweet = function () {
+    //message content selected using rng from a JSON
+    var tweetBody = randomProperty(facts);
+    //post content to twitter API
+    tweetThis(tweetBody)
 
 }
 
 
 
-
-
+followWorm();
 // grab & retweet as soon as program is running...
-retweet();
-// retweet in every 50 minutes
-setInterval(retweet, 300000);
+//retweet(queries.vapenation);
+// retweet at an interval
+//setInterval(retweet(queries.vapenation), 300000);
 
-randTweet();
-setInterval(randTweet,300000);
+//Start with random tweet
+//randTweet();
+// tweet another random tweet at an interval
+//setInterval(randTweet,300000);
